6d59748c1d851059e09c05727390c067962b25d6
vforge <bartosz.bentkowski@gmail.com>
2014-01-08 14:25:08 +0100

    Fix and bump ToC

--- main.lua
+++ main.lua
@@ -3,5 +3,6 @@ local addon, ns = ...
 ns.RegisterEvent("PLAYER_LOGIN", function()
 	SetCVar("CameraDistanceMax", "50")
 	SetCVar("cameraDistanceMaxFactor", "1")
+	
 	InterfaceOptionsCameraPanelMaxDistanceSlider:Disable();
 end)
